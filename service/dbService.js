e1c171113618899c8205290a4919b268845350d6
Eoin Halligan <ehalligan12@gmail.com>
2019-02-11 13:48:45 -0500

    cleaning up

--- service/dbService.js
+++ service/dbService.js
@@ -1,1 +1,24 @@
+let db = require('./db.js');
+
+
+/*
+Insert
+ */
+module.exports.insert = function(collection, documents, options) {
+    return new Promise((resolve, reject) => {
+        if(!collection) {
+            reject(new Error('Collection cannot be undefined'));
+        } else {
+            if(typeof documents != "object" && !documents.hasOwnProperty('length')) {
+                reject(new Error('Document must be of type array'));
+            } else {
+                collection.insertMany(documents, options).then((res) => {
+                    resolve(res);
+                }).catch((err) => {
+                    reject(err);
+                });
+            }
+        }
+    })
+};
 
